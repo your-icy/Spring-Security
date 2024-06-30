4efda1faa6488e4658ad27211bc0de1f54189122
your-icy <dev.harshpal@gmail.com>
2024-06-30 05:55:31 +0530

    "Config repaired"

--- src/main/java/com/icycodes/springsecurity/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/icycodes/springsecurity/event/listener/RegistrationCompleteEventListener.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import java.util.UUID;
 
 @Component
-@Slf4j
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
 
     @Autowired
